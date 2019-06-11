7f4b4e3542c4ce3c488aa2b7c96eb8bdc79102e0
Anmol Tiwari <32016777+anmol247@users.noreply.github.com>
2019-06-12 00:36:36 +0530

    Add files via upload

---
+++ Dino-Rexbot.py
@@ -0,0 +1,59 @@
+import pyautogui
+import time
+from PIL import ImageGrab , ImageOps
+from numpy import *
+
+class Coordinates:
+	restart_bttn = (340,390)
+	dino = (90,325)
+	box = (126,317,194,356)
+	restart_box = (315,294,366,337)
+	below_obs = (243,412,253,422)
+	above_obs = (243,387,253,399)
+
+
+def restart_game():
+	pyautogui.click(Coordinates.retart_bttn)
+
+def jump():
+	pyautogui.keyDown('space')
+	pyautogui.keyUp('space')
+	print("jump")
+
+def duck():
+    pyautogui.keyDown('down')
+    time.sleep(0.05)
+    pyautogui.keyUp('down')
+    print("duck")
+
+def imageGrab(box):
+    image   = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    sum = a.sum()
+    #print(sum)
+    return sum
+
+
+def is_game_over():
+    image = ImageGrab.grab(Coordinates.restart_box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    sum = a.sum()
+
+    if sum == 2934:
+        return False
+    else:
+        return True
+
+
+def main():
+    restart_game()
+    while True:
+        if imageGrab(Coordinates.below_obs) != 347:
+            jump()
+            continue
+        if imageGrab(Coordinates.above_obs) != 367:
+            duck()
+
+main()
